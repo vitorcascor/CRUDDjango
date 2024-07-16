a41cb174ea57a6392d535251932ec7517be775de
vytored <vytored2@gmail.com>
2024-07-16 13:13:31 -0300

    first commit

---
+++ todos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(verbose_name="Título",max_length=100, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateField(verbose_name="Data de Entrega",null=False, blank=False)
+    finished_at = models.DateField(null=True)
